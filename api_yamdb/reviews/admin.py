14a18af41a8d81b535ef393aa9058ff59d01b9b1
smaspb17 <121893680+smaspb17@users.noreply.github.com>
2023-05-24 15:18:07 +0300

    Update admin.py

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -3,15 +3,13 @@ from django.contrib import admin
 from .models import Comment, Review
 
 
+@admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('title', 'text', 'score',)
     search_fields = ('text',)
 
-
+    
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('title', 'review', 'text',)
     search_fields = ('text',)
-
-
-admin.site.register(Review, ReviewAdmin)
-admin.site.register(Comment, CommentAdmin)
