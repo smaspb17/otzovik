ef903cbe1872e37e3c84436ae6658a878d57a521
smaspb17 <121893680+smaspb17@users.noreply.github.com>
2023-05-24 15:24:12 +0300

    Update models.py

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -5,26 +5,28 @@ from datetime import datetime as dt
 
 
 class Category(models.Model):
-    name = models.CharField('Название', max_length=256)
-    slug = models.SlugField('Slug категории', unique=True)
+    name = models.CharField(verbose_name='Название', max_length=256)
+    slug = models.SlugField(verbose_name='Slug категории', unique=True)
 
     def __str__(self):
         return f'{self.name} ({self.pk})'
 
 
 class Genre(models.Model):
-    name = models.CharField('Название', max_length=256)
-    slug = models.SlugField('Slug жанра', unique=True)
+    name = models.CharField(verbose_name='Название', max_length=256)
+    slug = models.SlugField(verbose_name='Slug жанра', unique=True)
 
     def __str__(self):
         return f'{self.name} ({self.pk})'
 
 
 class Title(models.Model):
-    name = models.CharField('Название', max_length=256,)
+    name = models.CharField(verbose_name='Название', max_length=256,)
     year = models.PositiveIntegerField(
-        'Год выпуска', validators=[MaxValueValidator(dt.now().year)],)
-    description = models.TextField('Описание',)
+        verbose_name='Год выпуска',
+        validators=[MaxValueValidator(dt.now().year)],
+    )
+    description = models.TextField(verbose_name='Описание',)
     genre = models.ManyToManyField(Genres, verbose_name='Slug жанра',)
     category = models.ForeignKey(
         Categories, on_delete=models.SET_NULL, verbose_name='Slug категории',
@@ -40,7 +42,7 @@ class Review(models.Model):
         'Title', on_delete=models.CASCADE, verbose_name='ID произведения',
         related_name='reviews',
     )
-    text = models.TextField('Текст отзыва',)
+    text = models.TextField(verbose_name='Текст отзыва',)
     score = models.IntegerField(
         verbose_name='Оценка',
         validators=[MinValueValidator(1), MaxValueValidator(10)],
@@ -59,7 +61,7 @@ class Comment(models.Model):
         Review, on_delete=models.CASCADE, verbose_name='ID отзыва',
         related_name='comments',
     )
-    text = models.TextField('Текст комментария',)
+    text = models.TextField(verbose_name='Текст комментария',)
 
     def __str__(self):
         return f'{self.title} ({self.pk})'
