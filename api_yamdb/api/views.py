c8413de32af011b2ea2857369e61e3eb77612b69
ILYA-RS <123073243+ILYA-RS@users.noreply.github.com>
2023-05-24 22:07:29 +0300

    Add Category, Genre, Title urls, viewsets, admin site models (#4)

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    pass
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    pass
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    pass
--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Comment, Review
+from .models import Category, Comment, Genre, Review, Title
 
 
 @admin.register(Review)
@@ -8,8 +8,29 @@ class ReviewAdmin(admin.ModelAdmin):
     list_display = ('title', 'text', 'score',)
     search_fields = ('text',)
 
-    
+
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('title', 'review', 'text',)
     search_fields = ('text',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'year', 'description', 'category'
+    )
+    search_fields = ('name',)
+    list_filter = ('name', 'year', 'genre', 'category')
