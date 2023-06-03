9ef2a92b642d3743dbb6f59d803e228dd934253b
smaspb17 <smaspb17@yandex.ru>
2023-06-03 18:39:55 +0300

    Updated permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -9,6 +9,13 @@ class IsAdmin(BasePermission):
     pass
 
 
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return (request.method in SAFE_METHODS
+                or request.user.is_authenticated
+                and (request.user.is_admin or request.user.is_superuser))
+
+
 class IsAuthorOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
